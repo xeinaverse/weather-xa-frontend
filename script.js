489855ddb69150ea692aec6041d6a2a2124660d9
Zeina Tamer <zeina.tamer@cis.asu.edu.eg>
2020-09-30 11:28:16 +0200

    indentation fixed and date format changed

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let lati, long;
+let latitude, longitude;
 let today = new Date();
+
+const capitalizePhrase = (phrase) => {
+    return phrase
+      .toLowerCase()
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  };
+
 
 const getLocation = async () => {
     if (navigator.geolocation) {
@@ -7,12 +16,11 @@ const getLocation = async () => {
     } 
 }
 
-
 const showPosition = async (position) => {
 
-    lati = position.coords.latitude;
-    long = position.coords.longitude;
-    await sendDataToBackend('locationInfo', { lati, long });
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    await sendDataToBackend('locationInfo', { latitude, longitude });
 }
 
 const sendDataToBackend = async (endpointURL, data) => {
@@ -27,9 +35,8 @@ const sendDataToBackend = async (endpointURL, data) => {
         (data) => { return data.json(); }
     ).then(
         (data) => {
-
             document.getElementById('city').innerHTML = data.city;
-            document.getElementById('status').innerHTML = data.status;
+            document.getElementById('status').innerHTML = capitalizePhrase(data.status);
             document.getElementById('temp').innerHTML = data.temperature + '&#8451;';
             return data;
         }
@@ -38,6 +45,6 @@ const sendDataToBackend = async (endpointURL, data) => {
 
 const setTime = () => {
     document.getElementById('time').innerHTML =((today.getHours() < 10 ? '0' : '') + today.getHours()) + ":" + ((today.getMinutes() < 10 ? '0' : '') + today.getMinutes());
-    document.getElementById('date').innerHTML = today.toLocaleDateString("en-US", { weekday: 'long', month: 'long', day: 'numeric' });
+    document.getElementById('date').innerHTML = today.toLocaleDateString("en-US", { weekday: 'short', month: 'long', day: 'numeric' });
 }
 setTime();
